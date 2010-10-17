19393c51d5ff28106b7600594062f06558932b42
Kornelis Sietsma <korny@sietsma.com>
2010-10-17 18:41:01 +1100

    fixed namespace stuff - create namespace manually in window object

--- public/javascript/yamb.js
+++ public/javascript/yamb.js
@@ -1,99 +1,96 @@
 (function() {
-  var App, YAMB;
+  var YambApp;
   var __bind = function(func, context) {
     return function(){ return func.apply(context, arguments); };
   };
-  YAMB = {
-    App: (function() {
-      App = function() {
-        this.outputElement = '#output';
-        debug.log("binding");
-        $(window).hashchange(__bind(function(event) {
-          return this.hashchange(event);
-        }, this));
-        this.bindStateLinks();
-        $(window).hashchange();
-        return this;
+  YambApp = function() {
+    this.outputElement = '#output';
+    debug.log("binding");
+    $(window).hashchange(__bind(function(event) {
+      return this.hashchange(event);
+    }, this));
+    this.bindStateLinks();
+    $(window).hashchange();
+    return this;
+  };
+  YambApp.prototype.renderView = function(viewSel, data) {
+    var view;
+    view = Mustache.to_html($(viewSel).html(), data);
+    return $(this.outputElement).html(view);
+  };
+  YambApp.prototype.hashchange = function(event) {
+    var db, host, state;
+    host = event.getState('host');
+    db = event.getState('db');
+    debug.log("changed hash to host " + (host) + " db " + (db));
+    if (!(typeof host !== "undefined" && host !== null)) {
+      host = 'localhost';
+      state = {
+        host: host
       };
-      App.prototype.renderView = function(viewSel, data) {
-        var view;
-        view = Mustache.to_html($(viewSel).html(), data);
-        return $(this.outputElement).html(view);
-      };
-      App.prototype.hashchange = function(event) {
-        var db, host, state;
-        host = event.getState('host');
-        db = event.getState('db');
-        debug.log("changed hash to host " + (host) + " db " + (db));
-        if (!(typeof host !== "undefined" && host !== null)) {
-          host = 'localhost';
-          state = {
-            host: host
-          };
-          return $.bbq.pushState(state);
-        } else {
-          if (host && db) {
-            this.renderView("#db-view", {
-              hostname: host,
-              database: db,
-              loading: true
-            });
-            return this.loadDb(host, db);
-          } else {
-            this.renderView("#host-view", {
-              hostname: host,
-              database: '',
-              loading: true
-            });
-            return this.loadHost(host);
-          }
-        }
-      };
-      App.prototype.bindStateLinks = function() {
-        return $('#output a[href^=#]').live('click', function(e) {
-          var db, hostname, state;
-          hostname = $(this).attr("data-hostname");
-          db = $(this).attr("data-db");
-          debug.log("clicked host " + (hostname) + " db " + (db) + " - setting state");
-          state = {
-            host: hostname,
-            db: db
-          };
-          $.bbq.pushState(state);
-          return false;
+      return $.bbq.pushState(state);
+    } else {
+      if (host && db) {
+        this.renderView("#db-view", {
+          hostname: host,
+          database: db,
+          loading: true
         });
-      };
-      App.prototype.formatErrorState = function(textStatus, error) {
-        return {
-          message: textStatus
-        };
-      };
-      App.prototype.loadGeneric = function(url, viewSel) {
-        var that;
-        that = this;
-        return $.ajax({
-          url: url,
-          dataType: 'json',
-          data: null,
-          success: function(data) {
-            return data.success ? that.renderView(viewSel, data.payload) : that.renderView("#error-view", data.payload);
-          },
-          error: function(request, textStatus, error) {
-            return that.renderView("#error-view", that.formatErrorState(textStatus, error));
-          }
+        return this.loadDb(host, db);
+      } else {
+        this.renderView("#host-view", {
+          hostname: host,
+          database: '',
+          loading: true
         });
+        return this.loadHost(host);
+      }
+    }
+  };
+  YambApp.prototype.bindStateLinks = function() {
+    return $('#output a[href^=#]').live('click', function(e) {
+      var db, hostname, state;
+      hostname = $(this).attr("data-hostname");
+      db = $(this).attr("data-db");
+      debug.log("clicked host " + (hostname) + " db " + (db) + " - setting state");
+      state = {
+        host: hostname,
+        db: db
       };
-      App.prototype.loadHost = function(hostname) {
-        return this.loadGeneric("/" + (hostname) + ".json", "#host-view");
-      };
-      App.prototype.loadDb = function(hostname, db) {
-        return this.loadGeneric("/" + (hostname) + "/" + (db) + ".json", "#db-view");
-      };
-      return App;
-    })()
+      $.bbq.pushState(state);
+      return false;
+    });
+  };
+  YambApp.prototype.formatErrorState = function(textStatus, error) {
+    return {
+      message: textStatus
+    };
+  };
+  YambApp.prototype.loadGeneric = function(url, viewSel) {
+    var that;
+    that = this;
+    return $.ajax({
+      url: url,
+      dataType: 'json',
+      data: null,
+      success: function(data) {
+        return data.success ? that.renderView(viewSel, data.payload) : that.renderView("#error-view", data.payload);
+      },
+      error: function(request, textStatus, error) {
+        return that.renderView("#error-view", that.formatErrorState(textStatus, error));
+      }
+    });
+  };
+  YambApp.prototype.loadHost = function(hostname) {
+    return this.loadGeneric("/" + (hostname) + ".json", "#host-view");
+  };
+  YambApp.prototype.loadDb = function(hostname, db) {
+    return this.loadGeneric("/" + (hostname) + "/" + (db) + ".json", "#db-view");
   };
   $(function() {
-    window.YAMB = YAMB;
-    return (window.YambApp = new YAMB.App());
+    return (window.YAMB = {
+      App: YambApp,
+      the_app: new YambApp()
+    });
   });
 }).call(this);
--- views/coffee/yamb.coffee
+++ views/coffee/yamb.coffee
@@ -1,74 +1,73 @@
-# namespace
-YAMB =
-  # application
-  App: class App
-    constructor: ->
-      @outputElement = '#output'
-      # url-based state will include:
-      #  host: current host/port (or none if none selected)
-      #  db: current db (or none if none selected)
-      #  more as we get there...
+class YambApp
+  constructor: ->
+    @outputElement = '#output'
+    # url-based state will include:
+    #  host: current host/port (or none if none selected)
+    #  db: current db (or none if none selected)
+    #  more as we get there...
 
-      debug.log "binding"
-      $(window).hashchange (event) => @hashchange(event)
-      @bindStateLinks()
-      $(window).hashchange()
+    debug.log "binding"
+    $(window).hashchange (event) => @hashchange(event)
+    @bindStateLinks()
+    $(window).hashchange()
 
-    renderView: (viewSel,data) ->
-      view = Mustache.to_html($(viewSel).html(),data)
-      $(@outputElement).html(view)
+  renderView: (viewSel,data) ->
+    view = Mustache.to_html($(viewSel).html(),data)
+    $(@outputElement).html(view)
 
-    hashchange: (event) ->
-      host = event.getState 'host'
-      db = event.getState 'db'
-      debug.log "changed hash to host #{host} db #{db}"
-      # emulate old behaviour - for now - two states, one showing host, one showing db
-      #  - change view twice, once to show loading, once when loaded
-      unless host?
-        host = 'localhost'
-        state = {host:host}
-        $.bbq.pushState(state)
+  hashchange: (event) ->
+    host = event.getState 'host'
+    db = event.getState 'db'
+    debug.log "changed hash to host #{host} db #{db}"
+    # emulate old behaviour - for now - two states, one showing host, one showing db
+    #  - change view twice, once to show loading, once when loaded
+    unless host?
+      host = 'localhost'
+      state = {host:host}
+      $.bbq.pushState(state)
+    else
+      if host && db
+        @renderView("#db-view",{hostname: host, database: db, loading:true})
+        @loadDb(host,db)
       else
-        if host && db
-          @renderView("#db-view",{hostname: host, database: db, loading:true})
-          @loadDb(host,db)
+        @renderView("#host-view",{hostname: host, database: '', loading:true})
+        @loadHost(host)
+
+  bindStateLinks: ->
+    $('#output a[href^=#]').live 'click', (e) ->
+      hostname = $(this).attr("data-hostname")
+      db = $(this).attr("data-db")
+      debug.log "clicked host #{hostname} db #{db} - setting state"
+      state = {host:hostname, db:db}
+      $.bbq.pushState(state)
+      false
+
+  formatErrorState: (textStatus, error) ->
+    { message: textStatus }
+
+  loadGeneric: (url, viewSel) ->
+    that = this
+    $.ajax
+      url: url
+      dataType: 'json'
+      data: null
+      success: (data) ->
+        if data.success
+          that.renderView(viewSel,data.payload)
         else
-          @renderView("#host-view",{hostname: host, database: '', loading:true})
-          @loadHost(host)
+          that.renderView("#error-view",data.payload)
+      error: (request, textStatus, error) ->
+        that.renderView("#error-view", that.formatErrorState(textStatus,error))
 
-    bindStateLinks: ->
-      $('#output a[href^=#]').live 'click', (e) ->
-        hostname = $(this).attr("data-hostname")
-        db = $(this).attr("data-db")
-        debug.log "clicked host #{hostname} db #{db} - setting state"
-        state = {host:hostname, db:db}
-        $.bbq.pushState(state)
-        false
+  loadHost: (hostname) ->
+    @loadGeneric("/#{hostname}.json","#host-view")
 
-    formatErrorState: (textStatus, error) ->
-      { message: textStatus }
-
-    loadGeneric: (url, viewSel) ->
-      that = this
-      $.ajax
-        url: url
-        dataType: 'json'
-        data: null
-        success: (data) ->
-          if data.success
-            that.renderView(viewSel,data.payload)
-          else
-            that.renderView("#error-view",data.payload)
-        error: (request, textStatus, error) ->
-          that.renderView("#error-view", that.formatErrorState(textStatus,error))
-
-    loadHost: (hostname) ->
-      @loadGeneric("/#{hostname}.json","#host-view")
-
-    loadDb: (hostname,db) ->
-      @loadGeneric("/#{hostname}/#{db}.json","#db-view")
+  loadDb: (hostname,db) ->
+    @loadGeneric("/#{hostname}/#{db}.json","#db-view")
 
 $( ->
-  window.YAMB = YAMB
-  window.YambApp = new YAMB.App # expose globally for debugging
+  # expose a namespace for external use
+  window.YAMB =
+    App: YambApp
+    the_app: new YambApp  # create (and kick off) the application instance
 )
